bf4780650099ec2657ae4f03711070e8c3246148
dilpreetdhaliwal <dilpreet13dhaliwal@gmail.com>
2023-04-10 01:20:45 -0400

    added comments

--- script.js
+++ script.js
@@ -1,12 +1,15 @@
 
+
+// WELCOME PROMPT
     const helloParagraph = document.querySelector("#name");
     
-    const userName = prompt("What is your name?");
+    const userName = prompt("Welcome to the website, please enter your name");
     console.log(`${userName} is browsing the website`);
     
     helloParagraph.innerHTML = `Hello ${userName}! Quis autem vel eum iure reprehenderit qui in ea volupalate velit esse quah neil molesatia vel illum quie diloroem eum velit esse quam nihil consequator quie in ea el`;
 
 
+// BLOG CAROUSEL 
     document.querySelectorAll(".carousel").forEach(carousel => {
         const blogs = carousel.querySelectorAll(".blogContainer");
         const buttonsHtml = Array.from(blogs, () => {
@@ -32,12 +35,12 @@
             });
         });
 
+        // pick beginning position 
         blogs[0].classList.add("carouselItemSelected");
         buttons[0].classList.add("carouselButtonSelected");
     });
 
-
-
+// DROP DOWN ABOUT SECTION
     const aboutContainer = document.querySelector("#about");
     let isClicked = true;
 
